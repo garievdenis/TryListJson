222383f0dab0e73a3ac4d6a51f23ac0e3d822855
garievdenis <gariev@webmx.ru>
2022-09-16 13:59:03 +0500

    remove viewmodel and repo

--- app/src/main/java/com/example/trylistjson/ContactsViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.trylistjson
-
-import androidx.lifecycle.ViewModel
-
-class ContactsViewModel:ViewModel() {
-    private val repository = ContactsRepository.get()
-    val contactList = repository.contactList
-
-}
